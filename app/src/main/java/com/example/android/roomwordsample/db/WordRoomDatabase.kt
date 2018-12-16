1b7106a11ec201a9d219d9a2d703cf337786f7fb
DowgalOlya <dowgalolya@gmail.com>
2018-12-16 20:33:53 +0200

    Task 7 done

---
+++ app/src/main/java/com/example/android/roomwordsample/db/WordRoomDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.android.roomwordsample.db
+
+import android.arch.persistence.room.Database
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
+import android.content.Context
+import com.example.android.roomwordsample.db.interfaces.WordDao
+import com.example.android.roomwordsample.entities.Word
+
+@Database(entities = [Word::class], version = 1)
+abstract class WordRoomDatabase : RoomDatabase() {
+    abstract fun wordDao(): WordDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WordRoomDatabase? = null
+
+        fun getDatabase(context: Context): WordRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WordRoomDatabase::class.java,
+                    "Word_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
