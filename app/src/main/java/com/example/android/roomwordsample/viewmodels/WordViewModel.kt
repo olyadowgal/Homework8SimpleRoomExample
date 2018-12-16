941b642dde0689bc532ecbd61dd3591f46a40957
DowgalOlya <dowgalolya@gmail.com>
2018-12-16 22:04:36 +0200

    Task 10 done

---
+++ app/src/main/java/com/example/android/roomwordsample/viewmodels/WordViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.android.roomwordsample.viewmodels
+
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
+import android.arch.lifecycle.LiveData
+import com.example.android.roomwordsample.db.WordRoomDatabase
+import com.example.android.roomwordsample.entities.Word
+import com.example.android.roomwordsample.repositories.WordRepository
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+    private var parentJob = Job()
+    private val coroutineContext: CoroutineContext
+        get() = parentJob + Dispatchers.Main
+    private val scope = CoroutineScope(coroutineContext)
+
+    private val repository: WordRepository
+    val allWords: LiveData<List<Word>>
+
+    init {
+        val wordsDao = WordRoomDatabase.getDatabase(application).wordDao()
+        repository = WordRepository(wordsDao)
+        allWords = repository.allWords
+    }
+
+    fun insert(word: Word) = scope.launch(Dispatchers.IO) {
+        repository.insert(word)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        parentJob.cancel()
+    }
+}
